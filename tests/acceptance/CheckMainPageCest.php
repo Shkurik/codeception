7300056f1058e56d02f079a60aa790536930383b
Oleksandr Shkuropat <oleksandr.shkuropat@gmail.com>
2019-11-05 17:07:42 +0200

    add check main page

---
+++ tests/acceptance/CheckMainPageCest.php
@@ -0,0 +1,19 @@
+<?php
+
+class CheckMainPageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('For RUNNERS By RUNNERS', 'h1');
+    }
+}
